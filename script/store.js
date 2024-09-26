1126e3e44eaac5141d109dbe4a6adf069811da6b
04amanrajj <04.aman.raj@gmail.com>
2024-09-27 00:42:51 +0530

    Update: Store page added

---
+++ script/store.js
@@ -0,0 +1,83 @@
+let product = document.getElementById("products");
+product.innerHTML = `
+<div class="product">
+  <img src="/images/store/store-card-40-iphone-16-pro-202409_GEO_US.jpeg" alt="iPhone 16 Pro" />
+  <div class="product-content">
+    <h2>iPhone 16 Pro</h2>
+    <p>Hello, Apple Intelligence.</p>
+    <p class="price">From $1299 or $54.12/mo. for 24 mo.*</p>
+  </div>
+</div>
+<div class="product">
+  <img src="/images/store/store-card-40-watch-s10-202409.jpeg" alt="Apple Watch Series 10" height="200" width="300" />
+  <div class="product-content">
+    <h2>Apple Watch Series 10</h2>
+    <p>Thinnest classic.</p>
+    <p class="price">From $399 or $16.62/mo. for 24 mo.*</p>
+  </div>
+</div>
+<div class="product">
+  <img alt="iPhone 16" height="200" width="300" />
+  <div class="product-content">
+    <h2>iPhone 16</h2>
+    <p>Hello, Apple Intelligence.</p>
+    <p class="price">From $1099 or $45.79/mo. for 24 mo.*</p>
+  </div>
+</div>
+<div class="product">
+  <img alt="Apple Watch Ultra 2" height="200" width="300" />
+  <div class="product-content">
+    <h2>Apple Watch Ultra 2</h2>
+    <p>New finish. Never quit.</p>
+    <p class="price">From $799 or $33.29/mo. for 24 mo.*</p>
+  </div>
+</div>
+<div class="product">
+  <img alt="AirPods Pro" height="200" width="300" />
+  <div class="product-content">
+    <h2>AirPods Pro</h2>
+    <p>Rebuilt from the sound up.</p>
+    <p class="price">From $249 or $10.37/mo. for 24 mo.*</p>
+  </div>
+</div>
+<div class="product">
+  <img alt="iPhone 16 Pro" height="200" width="300" />
+  <div class="product-content">
+    <h2>iPhone 16 Pro</h2>
+    <p>Hello, Apple Intelligence.</p>
+    <p class="price">From $1299 or $54.12/mo. for 24 mo.*</p>
+  </div>
+</div>
+<div class="product">
+  <img alt="Apple Watch Series 10" height="200" width="300" />
+  <div class="product-content">
+    <h2>Apple Watch Series 10</h2>
+    <p>Thinnest classic.</p>
+    <p class="price">From $399 or $16.62/mo. for 24 mo.*</p>
+  </div>
+</div>
+<div class="product">
+  <img alt="iPhone 16" height="200" width="300" />
+  <div class="product-content">
+    <h2>iPhone 16</h2>
+    <p>Hello, Apple Intelligence.</p>
+    <p class="price">From $1099 or $45.79/mo. for 24 mo.*</p>
+  </div>
+</div>
+<div class="product">
+  <img alt="Apple Watch Ultra 2" height="200" width="300" />
+  <div class="product-content">
+    <h2>Apple Watch Ultra 2</h2>
+    <p>New finish. Never quit.</p>
+    <p class="price">From $799 or $33.29/mo. for 24 mo.*</p>
+  </div>
+</div>
+<div class="product">
+  <img alt="AirPods Pro" height="200" width="300" />
+  <div class="product-content">
+    <h2>AirPods Pro</h2>
+    <p>Rebuilt from the sound up.</p>
+    <p class="price">From $249 or $10.37/mo. for 24 mo.*</p>
+  </div>
+</div>
+`;
