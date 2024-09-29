0daa99ca950460dbcb33b1883c0c9776f85a69dc
04amanrajj <04.aman.raj@gmail.com>
2024-09-29 17:02:27 +0530

    Update: single page added

--- script/detail.js
+++ script/detail.js
@@ -4,14 +4,29 @@ header();
 footer();
 slide();
 
+// main code
+
 let id = localStorage.getItem("id");
 let baseUrl = "http://localhost:4000/products/" + id;
-let iphones = document.querySelector(".iphones");
+let productImg = document.querySelector(".device-img img");
+let productName = document.getElementsByClassName("name");
+let topPrice = document.querySelector(".price");
+let localData;
 
-async function getInfo(url) {
-  let data = await fetch(url);
-  data = await data.json();
-  console.log(data)
+try {
+  const response = await fetch(baseUrl);
+  const result = await response.json();
+  console.log(result);
+  localData = result;
+} catch (error) {
+  console.log(error);
 }
-getInfo(baseUrl);
+console.log(productName);
+productImg.src = localData.image;
+for (let i of productName) {
+  i.textContent = localData.title;
+}
 
+topPrice.innerHTML = `From $${localData.price} or $${Math.floor(
+  localData.price / 24
+)}/mo. per month for 24mo.months`
